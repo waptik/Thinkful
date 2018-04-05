397f4e9e9031107d6300aba4d4bc97876f210ad7
Stephane Mensah <stephane.mensah@st.vvu.edu.gh>
2018-04-05 19:01:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Thinkful
+Anything related to Thinkful's Frontend Web Development course through GitHub's education package.
---
+++ fibonacci_sequence.js
@@ -0,0 +1,91 @@
+// noprotect
+// ^^ `noprotect` is here to fix a bug with JSBIn
+
+
+function getFibListLength() {
+  // we're stubbing this function,
+  // which means having it return a
+  // temporary, hard-coded value
+  // that is representative of a 
+  // typical value this function
+  // might return. That way we can 
+  // focus on implementing the 
+  // unique part of this problem:
+  // generating Fibonacci numbers
+  return 20;
+}
+
+// this is a function. it's a named
+// set of instructions that you can
+// call elsewhere in your code. this
+// function is called `generateFib`
+// and when you call it, you pass it
+// a number (`num`)
+function generateFib(num) {
+  // this creates an empty array.
+  // you'll learn about array later
+  // in this course
+  var result = []; 
+  // this is called a *for loop*.
+  // a for loop executes a block of 
+  // instructions a set number of 
+  // times. this loop will execute
+  // however many times the `num`       // variable says to do.
+  for (var i = 1; i <= num; i++) {
+    // here between the { } brackets
+    // are the set of instructions
+    // that get completed each time
+    // through the for loop.
+    // this particular set of
+    // instructions says that if
+    // we're adding the first item
+    // to the result list, append the
+    // number 0 to results
+    if (i === 1) {
+      result.push(0);
+    }
+    // ...and if it's the second item
+    // append 1
+    else if (i == 2) {
+      result.push(1);
+    }
+    // otherwise, sum the two previous result items, and append that value to results.
+    else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  // once the for loop finishes
+  // we return `result`.
+  return result;
+}
+
+// this function is responsible
+// for displaying Fibonacci
+// numbers in the DOM. It appends
+// each result item as a separate `<p>` element
+function displayFibs(fibs) {
+  var mainEl = document.getElementsByTagName('main')[0];
+  for (var i=0; i < fibs.length; i++) {
+    console.log(fibs[i]);
+    var newEl = document.createElement('p');
+    newEl.innerText = fibs[i];
+    mainEl.appendChild(newEl);
+  }
+}
+
+// we define a `main` function that
+// is responsible for calling
+// the three other functions
+// we called, in the right order,
+// and routing outputs from one
+// function into another.
+function main() {
+  var fibLimit = getFibListLength();
+  var fibs = generateFib(fibLimit);
+  displayFibs(fibs);
+}
+
+// once the page has fully loaded,
+// the function between the `()` parens runs.
+// you'll learn more about this later.
+$(main);
